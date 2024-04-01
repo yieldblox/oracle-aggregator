c9b82ef11bd191c4055cf484fc9a5c3acfe543a8
Ryang-21 <ryanyang2001@gmail.com>
2024-04-01 15:16:01 -0500

    fix time check on circuit breaker and scale velocity threshold to 5 decimals

--- oracle-aggregator/src/circuit_breaker.rs
+++ oracle-aggregator/src/circuit_breaker.rs
@@ -13,10 +13,10 @@ pub fn check_valid_velocity(
 ) -> bool {
     let velocity_threshold = storage::get_velocity_threshold(&e);
     if prev_price.timestamp < price.timestamp {
-        let velocity = ((price.price - prev_price.price).abs() * 10000
-            / prev_price.price
-            / (price.timestamp - prev_price.timestamp) as i128) as u32;
-        if velocity > velocity_threshold {
+        let percent_price_delta = (price.price - prev_price.price).abs() * 10000 / prev_price.price;
+        let velocity =
+            percent_price_delta * 10000 / (price.timestamp - prev_price.timestamp) as i128;
+        if velocity as u32 > velocity_threshold {
             let circuit_breaker_timeout = storage::get_timeout(&e);
             storage::set_circuit_breaker_status(&e, &asset, &true);
             storage::set_circuit_breaker_timeout(
@@ -35,7 +35,7 @@ pub fn check_valid_velocity(
 
 pub fn check_circuit_breaker(e: &Env, asset: &Asset) {
     if storage::has_circuit_breaker(&e) && storage::get_circuit_breaker_status(&e, &asset) {
-        if storage::get_timeout(&e) < e.ledger().timestamp() {
+        if storage::get_circuit_breaker_timeout(&e, &asset) > e.ledger().timestamp() {
             panic_with_error!(&e, OracleAggregatorErrors::CircuitBreakerTripped);
         } else {
             storage::set_circuit_breaker_status(&e, &asset, &false);
