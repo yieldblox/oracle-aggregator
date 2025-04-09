45cec780783817bcf4d4da8dd7127e5ba0521b29
mootz12 <mootz12@protonmail.com>
2025-04-09 10:16:49 -0400

    chore: remove rustdoc comments to avoid WASM size increase

--- src/contract.rs
+++ src/contract.rs
@@ -11,16 +11,16 @@ pub struct OracleAggregator;
 
 #[contractimpl]
 impl OracleAggregator {
-    /// Initialize the oracle aggregator contract.
-    ///
-    /// ### Arguments
-    /// * `admin` - The address of the admin
-    /// * `base` - The address of the base asset the oracle will report in
-    /// * `decimals` - The decimals the oracle will report in
-    /// * `max_age` - The maximum time the oracle will look back for a price (in seconds)
-    ///
-    /// ### Errors
-    /// * `InvalidMaxAge` - The max age is not between 360 (6m) and 3600 (60m)
+    // Initialize the oracle aggregator contract.
+    //
+    // ### Arguments
+    // * `admin` - The address of the admin
+    // * `base` - The address of the base asset the oracle will report in
+    // * `decimals` - The decimals the oracle will report in
+    // * `max_age` - The maximum time the oracle will look back for a price (in seconds)
+    //
+    // ### Errors
+    // * `InvalidMaxAge` - The max age is not between 360 (6m) and 3600 (60m)
     pub fn __constructor(e: Env, admin: Address, base: Asset, decimals: u32, max_age: u64) {
         storage::extend_instance(&e);
         storage::set_admin(&e, &admin);
@@ -34,32 +34,32 @@ impl OracleAggregator {
 
     /**** Read Only *****/
 
-    /// Fetch the supported oracles
+    // Fetch the supported oracles
     pub fn oracles(e: Env) -> Vec<OracleConfig> {
         storage::get_oracles(&e)
     }
 
-    /// Fetch the configuration of an asset
+    // Fetch the configuration of an asset
     pub fn asset_configs(e: Env) -> Map<Asset, AssetConfig> {
         storage::get_asset_configs(&e)
     }
 
-    /// Fetch the max age of a price
+    // Fetch the max age of a price
     pub fn max_age(e: Env) -> u64 {
         storage::get_max_age(&e)
     }
 
-    /// Fetch the base asset
+    // Fetch the base asset
     pub fn base(e: Env) -> Asset {
         storage::get_base(&e)
     }
 
-    /// Fetch the decimals the oracle will report in
+    // Fetch the decimals the oracle will report in
     pub fn decimals(e: Env) -> u32 {
         storage::get_decimals(&e)
     }
 
-    /// Fetch the list of assets the oracle supports
+    // Fetch the list of assets the oracle supports
     pub fn assets(e: Env) -> Vec<Asset> {
         let asset_configs = storage::get_asset_configs(&e);
         let mut base_assets = storage::get_base_assets(&e);
@@ -67,18 +67,18 @@ impl OracleAggregator {
         base_assets
     }
 
-    /// Fetch the last price of the Asset based on the asset config.
-    ///
-    /// ### Arguments
-    /// * `asset` - The asset to fetch the price for
-    ///
-    /// ### Returns
-    /// * The lastprice from the source oracle
-    /// * None if the price cannot be resolved, or is outside the configured bounds
-    ///
-    /// ### Errors
-    /// * `AssetNotFound` - The asset is not in the list of assets
-    /// * `OracleNotFound` - The oracle is not in the list of oracles
+    // Fetch the last price of the Asset based on the asset config.
+    //
+    // ### Arguments
+    // * `asset` - The asset to fetch the price for
+    //
+    // ### Returns
+    // * The lastprice from the source oracle
+    // * None if the price cannot be resolved, or is outside the configured bounds
+    //
+    // ### Errors
+    // * `AssetNotFound` - The asset is not in the list of assets
+    // * `OracleNotFound` - The oracle is not in the list of oracles
     pub fn lastprice(e: Env, asset: Asset) -> Option<PriceData> {
         storage::extend_instance(&e);
 
@@ -106,22 +106,22 @@ impl OracleAggregator {
 
     /***** Admin Functions *****/
 
-    /// (Admin Only) Set the admin address
-    ///
-    /// ### Arguments
-    /// * `admin` - The address of the new admin
+    // (Admin Only) Set the admin address
+    //
+    // ### Arguments
+    // * `admin` - The address of the new admin
     pub fn set_admin(e: Env, admin: Address) {
         storage::get_admin(&e).require_auth();
         storage::set_admin(&e, &admin);
     }
 
-    /// (Admin Only) Add an oracle to the oracle aggregator
-    ///
-    /// ### Arguments
-    /// * `oracle_id` - The address of the oracle
-    ///
-    /// ### Errors
-    /// * `OracleAlreadyExists` - The oracle already exists
+    // (Admin Only) Add an oracle to the oracle aggregator
+    //
+    // ### Arguments
+    // * `oracle_id` - The address of the oracle
+    //
+    // ### Errors
+    // * `OracleAlreadyExists` - The oracle already exists
     pub fn add_oracle(e: Env, oracle_id: Address) {
         storage::get_admin(&e).require_auth();
         storage::extend_instance(&e);
@@ -146,22 +146,22 @@ impl OracleAggregator {
         storage::set_oracles(&e, &oracles);
     }
 
-    /// (Admin Only) Add an asset to the oracle aggregator
-    ///
-    /// ### Arguments
-    /// * `asset` - The asset to add
-    /// * `oracle_id` - The address of the oracle
-    /// * `oracle_asset` - The asset used to fetch the oracle price
-    /// * `max_dev` - The maximum deviation allowed for a price, as a percentage with 0 decimals. If this is 0,
-    ///               the oracle will just fetch the last price within the resolution time.
-    ///
-    /// ### Errors
-    /// * `InvalidAssetOracle` - Unable to fetch a price for the oracle asset
-    /// * `AssetExists` - The asset already exists
-    ///
-    /// ### Returns
-    /// The price data of the asset as would be returned by `self.lastprice`. This is useful
-    /// for simulation to verify the asset was added correctly.
+    // (Admin Only) Add an asset to the oracle aggregator
+    //
+    // ### Arguments
+    // * `asset` - The asset to add
+    // * `oracle_id` - The address of the oracle
+    // * `oracle_asset` - The asset used to fetch the oracle price
+    // * `max_dev` - The maximum deviation allowed for a price, as a percentage with 0 decimals. If this is 0,
+    //               the oracle will just fetch the last price within the resolution time.
+    //
+    // ### Errors
+    // * `InvalidAssetOracle` - Unable to fetch a price for the oracle asset
+    // * `AssetExists` - The asset already exists
+    //
+    // ### Returns
+    // The price data of the asset as would be returned by `self.lastprice`. This is useful
+    // for simulation to verify the asset was added correctly.
     pub fn add_asset(
         e: Env,
         asset: Asset,
@@ -214,11 +214,11 @@ impl OracleAggregator {
         }
     }
 
-    /// (Admin Only) Add an asset that reports a price of 1 base asset. This should be used
-    /// sparingly and only for assets that are redeemable for the base asset, and no safe oracle exists.
-    ///
-    /// ### Arguments
-    /// * `base` - The asset to add
+    // (Admin Only) Add an asset that reports a price of 1 base asset. This should be used
+    // sparingly and only for assets that are redeemable for the base asset, and no safe oracle exists.
+    //
+    // ### Arguments
+    // * `base` - The asset to add
     pub fn add_base_asset(e: Env, base: Asset) {
         storage::get_admin(&e).require_auth();
         storage::extend_instance(&e);
